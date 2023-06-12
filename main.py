06f1d232cd4a78a8a4b13211405804ac623e463d
Fenekito <juanccaires@gmail.com>
2023-06-12 16:55:01 -0300

    Finalização

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from itertools import cycle
-from processo import Processo
 from random import randint
 
-maxPages = 10
+maxPages = 3
+
+"""
+FEITO POR:
+JUAN CLAUDIO
+JORGE LUCAS
+RUAN GOMES
+"""
 
 def clock(n, ref_list):
     pages = [-1] * n  # Initialize memory slots with -1 (empty)
@@ -11,7 +17,7 @@ def clock(n, ref_list):
     bits = 0  # Variable to store the bits
 
     for p in ref_list:
-        if p.pageId in d:  # Page already in memory
+        if p in d:  # Page already in memory
             bits |= d[p]  # Set the corresponding bit for the page
         else:  # Page fault
             for i, b in idx:
@@ -28,32 +34,36 @@ def clock(n, ref_list):
     return pages
 
 def mru(n, ref_list):
-    cache, time = [-1] * n, [-1] * n
+    cache, time, pids = [-1] * n, [-1] * n, [-1] * n
     for i, x in enumerate(ref_list):
-        idx = cache.index(x) if x.pageId in cache else time.index(min(time))
+        idx = cache.index(x) if x in cache else time.index(min(time))
         cache[idx] = x
         time[idx] = i
     return cache
 
 def fifo(n, ref_list):
     fList = []
-    pIds = [p.pageId for p in ref_list]
     counter = 0
     for i in ref_list:
         if len(fList) < n and not i in fList:
             fList.append(i)
+            print(f"Memória livre e Pagina {i} não está na memoria")
         else:
             if not i in fList:
                 fList[counter] = i
+                print("Page Fault!")
                 counter += 1
                 if counter >= len(fList): counter = 0
     if ref_list == []:
         return [-1 for x in range(n)]
     while len(fList) < n:
-        #endereco vazio
+        print("Há Espaços Vazios na memoria")
         fList.append(-1)
 
+    print(ref_list)
+    print(fList)
     return fList
+
 
 def sc(n, ref_list):
     pages = [-1] * n  # Inicializa os slots de memória com -1 (vazio)
@@ -76,26 +86,52 @@ def sc(n, ref_list):
 
     return pages
 
-processos = []
+def nur(n, ref_list):
+    pages = [-1] * n  # Initialize memory slots with -1 (empty)
+    ref_bits = [0] * n  # Reference bit for each memory slot
+    mod_bits = [0] * n  # Modification bit for each memory slot
+    pointer = 0  # Pointer to the next available memory slot
+
+    for p in ref_list:
+        if p in pages:  # Page already in memory
+            ref_bits[pages.index(p)] = 1  # Set the reference bit to 1
+        else:  # Page fault (page not in memory)
+            while True:
+                if ref_bits[pointer] == 0 and mod_bits[pointer] == 0:  # Found a page with both reference and modification bits equal to 0
+                    pages[pointer] = p  # Replace the page
+                    ref_bits[pointer] = 1  # Set the reference bit to 1
+                    mod_bits[pointer] = 0  # Set the modification bit to 0
+                    pointer = (pointer + 1) % n  # Move to the next memory slot
+                    break
+                else:
+                    ref_bits[pointer] = 0  # Set the reference bit to 0
+                    mod_bits[pointer] = 1  # Set the modification bit to 1
+                    pointer = (pointer + 1) % n  # Move to the next memory slot
+
+    return pages
+
+paginas = []
 for i in range(10):
-    p = randint(1, maxPages)
-    processos.append(Processo(p))
-    print(processos[i].id)
+    p = randint(1, 5)
+    paginas.append(p)
+
+print(f"Id das paginas: {paginas}")
 
 if __name__ == '__main__':
+    print(f"Será Simulado uma memória virtual com {maxPages} de páginas")
+    print("As páginas vão do endereço 1 - 5")
     print("Escolha uma opção:")
     print("[1]FIFO\n[2]MRU\n[3]CLOCK\n[4]SC\n[5]NUR\n")
     match input():
         case "1":
-            processos = fifo(maxPages, processos)
+            paginas = fifo(maxPages, paginas)
         case "2":
-            processos = mru(maxPages, processos)
+            paginas = mru(maxPages, paginas)
         case "3":
-            processos = clock(maxPages, processos)
+            paginas = clock(maxPages, paginas)
         case "4":
-            processos = sc(maxPages, processos)
+            paginas = sc(maxPages, paginas)
         case "5":
-            pass
+            paginas = nur(maxPages, paginas)
 
-    for p in processos:
-        print(p.id)
+    print(f"Paginas em memoria: {paginas}")
--- processo.py
+++
@@ -1,5 +0,0 @@
-import uuid
-
-class Processo:
-    def __init__(self):
-        self.id = uuid.uuid4()
